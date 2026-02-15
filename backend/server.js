c67c796199bf829c18cb3cc5fba83aa803931302
Galang Dewangkara <galanggit23@gmail.com>
2026-02-16 00:02:40 +0700

    fix: update server js

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,9 @@ require('dotenv').config();
 
 const app = express();
 
-// Middleware
-// --- KONFIGURASI CORS ---
+// --- MIDDLEWARE ---
+app.use(express.json());
+
 const allowedOrigins = [
   'http://localhost:5173',
   'https://diy-karang-taruna-webapp.vercel.app',
@@ -26,20 +27,22 @@ app.use(
     credentials: true,
   }),
 );
-// Koneksi ke MongoDB
+
+// --- KONEKSI MONGODB ---
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('✅ Berhasil terhubung ke MongoDB'))
   .catch((err) => console.error('❌ Gagal koneksi ke MongoDB:', err));
 
-// Route Dasar (Testing)
+// --- ROUTES ---
 app.get('/', (req, res) => {
   res.send('API DIY Carpentry Karang Taruna Running...');
 });
+
 app.use('/api/auth', require('./routes/authRoutes.js'));
 app.use('/api/tutorials', require('./routes/tutorialRoutes.js'));
 app.use('/api/users', require('./routes/userRoutes.js'));
-// Menjalankan Server [cite: 31, 33]
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
